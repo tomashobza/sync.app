4d36969d05237041d14d19fa754388d400c3aa92
Nikita Koliada <xkolia00@vutbr.cz>
2023-10-24 19:52:02 +0200

    add: user token stores

--- client/src/ts/stores.ts
+++ client/src/ts/stores.ts
@@ -1,13 +1,18 @@
-import { writable } from "svelte/store";
+import { writable } from 'svelte/store';
+import type { User } from 'firebase/auth';
 
+export const user_token = writable<User | null>(null);
 
 export const user_data = writable<{
-    name: string;
-    id: string;
-    email: string;
-} | null>({
-    name: "Anastasia Butok",
-    id: "0",
-    email: "xbutok00@vutbr.cz",
-});
+	name: string;
+	id: string;
+	email: string;
+} | null>();
 
+user_token.subscribe((value) => {
+	user_data.set({
+		name: value?.displayName || 'unknown',
+		id: value?.uid || '0',
+		email: value?.email || ''
+	});
+});
