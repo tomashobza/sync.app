4a5a17274b0c266be00c7da6eb1eb8e62e8f588e
Tomáš Hobza <thomashobza@ids.company>
2023-10-24 19:48:14 +0200

    add: onclick event on button

--- client/src/lib/Button.svelte
+++ client/src/lib/Button.svelte
@@ -1,5 +1,6 @@
 <button
 	class="bg-primary font-semibold px-3 py-2 rounded-full min-w-[10rem] shadow-md transition-all hover:scale-95 focus:scale-95 active:scale-90"
+	on:click
 >
 	<slot />
 </button>
