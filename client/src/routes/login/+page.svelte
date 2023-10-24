96b77e2f5768b7fc639046037e181f6ca435ea88
Anastasia Butok <xbutok00@vutbr.cz>
2023-10-24 19:51:31 +0200

    add: login logic to login route

--- client/src/routes/login/+page.svelte
+++ client/src/routes/login/+page.svelte
@@ -1,9 +1,28 @@
-<script>
+<script lang="ts">
 	import Logo from '$lib/Logo.svelte';
 	import Input from '$lib/Input.svelte';
 	import EnvelopeIcon from '$lib/EnvelopeIcon.svelte';
 	import Button from '$lib/Button.svelte';
 	import LockIcon from '$lib/LockIcon.svelte';
+	import { login } from '$ts/login';
+	import toast from 'svelte-french-toast';
+	import { user_token } from '$ts/stores';
+	import { goto } from '$app/navigation';
+
+	const handleLogin = () => {
+		toast.promise(login(email, password), {
+			loading: 'Logging in...',
+			success: 'Log in successful!',
+			error: 'Could not log in.'
+		});
+	};
+
+	$: if ($user_token) {
+		goto('/');
+	}
+
+	let email: string;
+	let password: string;
 </script>
 
 <div class="w-full h-full flex flex-col items-center justify-center p-6 text-white">
@@ -12,14 +31,15 @@
 	</div>
 	<div class="text-2xl font-bold">Project planner</div>
 	<div class="flex flex-col gap-4 text-[#2f2f2f] my-8 w-full max-w-[18rem]">
-		<Input placeholder="Email address">
+		<Input placeholder="Email address" bind:value={email}>
 			<EnvelopeIcon />
 		</Input>
-		<Input placeholder="Password" type="password">
+		<Input placeholder="Password" type="password" bind:value={password}>
 			<LockIcon />
 		</Input>
 		<!-- <input type="email" placeholder="Email address:" />
 			<input type="password" /> -->
 	</div>
-	<Button>Log in</Button>
+	<Button on:click={handleLogin}>Log in</Button>
+	<button class="py-2 mt-4 underline" on:click={() => goto('/login/new')}>Create an account</button>
 </div>
