7cc5e37649ac5bef1933f7fe9cfbfd9db68f135d
Tomáš Hobza <thomashobza@ids.company>
2023-10-25 13:29:36 +0200

    add: header shadow for support

--- client/src/lib/Header.svelte
+++ client/src/lib/Header.svelte
@@ -4,9 +4,16 @@
 
 	export let scroll: number = 0;
 	$: blur_val = Math.min(scroll / 20, 5);
+	$: bg_val = Math.min(scroll / 50, 8);
+
+	$: console.log(bg_val - 1);
 </script>
 
-<div class="header" style="backdrop-filter: blur({blur_val}px)">
+<div
+	class="header"
+	style="backdrop-filter: blur({blur_val}px); background: linear-gradient(rgba(0,0,0,{(bg_val - 1) /
+		10}), #00000000)"
+>
 	<div>{$user_data?.name ?? 'unknown'}</div>
 	<button on:click>
 		<Burger />
