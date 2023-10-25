a5f80175f1f46062ddafa119742067b229cde4ac
Tomáš Hobza <thomashobza@ids.company>
2023-10-25 14:21:54 +0200

    add: better color Header overlay

--- client/src/lib/Header.svelte
+++ client/src/lib/Header.svelte
@@ -5,13 +5,12 @@
 	export let scroll: number = 0;
 	$: blur_val = Math.min(scroll / 20, 5);
 	$: bg_val = Math.min(scroll / 50, 8);
-
-	$: console.log(bg_val - 1);
 </script>
 
 <div
 	class="header"
-	style="backdrop-filter: blur({blur_val}px); background: linear-gradient(rgba(0,0,0,{(bg_val - 1) /
+	style="backdrop-filter: blur({blur_val}px); background: linear-gradient(rgba(52	,18, 78, {(bg_val -
+		1) /
 		10}), #00000000)"
 >
 	<div>{$user_data?.name ?? 'unknown'}</div>
