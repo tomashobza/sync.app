d62d8b9053a97a1075aae0fafebe1d811b4f6c5f
Nikita Koliada <xkolia00@vutbr.cz>
2023-10-25 13:29:50 +0200

    add: ProgressBar component withou text

---
+++ client/src/lib/ProgressBar.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let value = 0;
+	export let color = '#51E5FF';
+</script>
+
+<div class="w-full h-full rounded-full border border-[#2f2f2f] overflow-hidden">
+	<div
+		class="h-full border-r border-[#2f2f2f]"
+		style="width: {value * 100}%; background-color: {color}"
+	/>
+</div>
