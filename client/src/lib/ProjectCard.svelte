6c15a96d6b9b4e449faa2e328da9fc1c733ca5c9
Tomáš Hobza <xhobza03@vutbr.cz>
2023-10-26 16:32:14 +0200

    add: transition to project card

--- client/src/lib/ProjectCard.svelte
+++ client/src/lib/ProjectCard.svelte
@@ -2,33 +2,41 @@
 	import type { Project } from '$ts/interfaces';
 	import dayjs from 'dayjs';
 	import ProgressBar from './ProgressBar.svelte';
+	import { scale } from 'svelte/transition';
+	import { onMount } from 'svelte';
 
 	const colors = ['#51E5FF', '#FDE74C', '#EC368D'];
 	export let i = 0;
 
 	export let project: Project;
+
+	let showCard = false;
+	onMount(() => (showCard = true));
 </script>
 
-<div
-	class="w-full max-w-[22rem] p-6 bg-white rounded-3xl flex flex-col items-stretch gap-2 text-[#2F2F2F]"
->
-	<div class="flex flex-row items-center">
-		<div class="flex-grow text-3xl font-bold">
-			{project?.name ?? 'unknown'}
+{#if showCard}
+	<div
+		class="w-full max-w-[22rem] p-6 bg-white rounded-3xl flex flex-col items-stretch gap-2 text-[#2F2F2F] card"
+		in:scale={{ delay: i * 50 }}
+	>
+		<div class="flex flex-row items-center">
+			<div class="flex-grow text-3xl font-bold">
+				{project?.name ?? 'unknown'}
+			</div>
+			<div
+				class="text-sm px-2 py-1 rounded-full border border-[#2f2f2f]"
+				style="background-color: {colors[i % colors.length]}"
+			>
+				{dayjs(project?.duedate).format('DD/MM/YYYY') ?? 'unknown'}
+			</div>
 		</div>
-		<div
-			class="text-sm px-2 py-1 rounded-full border border-[#2f2f2f]"
-			style="background-color: {colors[i % colors.length]}"
-		>
-			{dayjs(project?.duedate).format('DD/MM/YYYY') ?? 'unknown'}
+		<div class="flex-grow text-xs flex flex-col gap-1 mb-1">
+			{#each project?.members ?? [] as member}
+				<div>{member?.name ?? 'unknown'}</div>
+			{/each}
+		</div>
+		<div class="w-full h-6">
+			<ProgressBar value={0.6} color={colors[i % colors.length]} />
 		</div>
 	</div>
-	<div class="flex-grow text-xs flex flex-col gap-1 mb-1">
-		{#each project?.members ?? [] as member}
-			<div>{member?.name ?? 'unknown'}</div>
-		{/each}
-	</div>
-	<div class="w-full h-6">
-		<ProgressBar value={0.6} color={colors[i % colors.length]} />
-	</div>
-</div>
+{/if}
