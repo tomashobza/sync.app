825b03bd73aad374731160954c5c255923ff9b5b
Tomáš Hobza <thomashobza@ids.company>
2023-11-04 22:59:46 +0100

    add: ProgressBar component

--- client/src/lib/ProgressBar.svelte
+++ client/src/lib/ProgressBar.svelte
@@ -3,9 +3,17 @@
 	export let color = '#51E5FF';
 </script>
 
-<div class="w-full h-full rounded-full border border-[#2f2f2f] overflow-hidden">
+<div class="z-0 w-full h-full relative rounded-full border border-[#2f2f2f] overflow-hidden">
 	<div
-		class="h-full border-r border-[#2f2f2f]"
+		class="-z-10 h-full border-r border-[#2f2f2f]"
 		style="width: {value * 100}%; background-color: {color}"
 	/>
+
+	<div
+		class="-z-10 absolute inset-0 flex flex-row items-center justify-end px-2 text-xs"
+		class:text-black={color != '#fff'}
+		class:text-white={color == '#fff'}
+	>
+		{Math.round(value * 100)}%
+	</div>
 </div>
