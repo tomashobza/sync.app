6359a71fad244da1229da5e6f6271744307b82a8
Nikita Koliada <xkolia00@vutbr.cz>
2023-11-04 22:58:41 +0100

    add: updated Button component

--- client/src/lib/Button.svelte
+++ client/src/lib/Button.svelte
@@ -1,5 +1,13 @@
+<script lang="ts">
+	export let hasMinWidth: boolean = true;
+	export let color: string = 'primary';
+</script>
+
 <button
-	class="bg-primary font-semibold px-3 py-2 rounded-full min-w-[10rem] shadow-md transition-all hover:scale-95 focus:scale-95 active:scale-90"
+	class=" font-semibold px-5 py-3 rounded-full shadow-md transition-all hover:scale-95 focus:scale-95 active:scale-90 text-white"
+	class:bg-primary={color == 'primary'}
+	style={color != 'primary' ? `background-color: ${color}` : ''}
+	class:min-w-[10rem]={hasMinWidth}
 	on:click
 >
 	<slot />
