a77137ebd61aaae847195644cf4fe1589959dd5f
Tomáš Hobza <thomashobza@ids.company>
2023-11-04 23:01:37 +0100

    add: interfaces

--- client/src/ts/interfaces.ts
+++ client/src/ts/interfaces.ts
@@ -1,12 +1,17 @@
+import type { DocumentData, Timestamp } from 'firebase/firestore';
+
 export interface Project {
 	name: string;
 	id: string;
 	members: Member[];
+	links: string[];
 	duedate: Date;
 }
 
-export interface Member {
-	name: string;
-	id: string;
+export interface Member extends DocumentData {
+	displayName: string;
+	uid: string;
 	email: string;
+	photoURL: string;
+	createdAt: Timestamp;
 }
