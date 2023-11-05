287e20d2f83e25cdf1a0cf0f12bfcbe387dbdfae
Tomáš Hobza <thomashobza@ids.company>
2023-11-05 12:53:39 +0100

    add: progress bar value color and transition

--- client/src/lib/ProgressBar.svelte
+++ client/src/lib/ProgressBar.svelte
@@ -5,14 +5,17 @@
 
 <div class="z-0 w-full h-full relative rounded-full border border-[#2f2f2f] overflow-hidden">
 	<div
-		class="-z-10 h-full border-r border-[#2f2f2f]"
+		class="-z-10 h-full border-r border-[#2f2f2f] transition-all duration-300"
 		style="width: {value * 100}%; background-color: {color}"
 	/>
 
+	<!-- <div
+		class="z-0 absolute inset-0 flex flex-row items-center bg-blend-color justify-end px-2 text-xs text-[#2f2f2f]"
+	>
+		{Math.round(value * 100)}%
+	</div> -->
 	<div
-		class="-z-10 absolute inset-0 flex flex-row items-center justify-end px-2 text-xs"
-		class:text-black={color != '#fff'}
-		class:text-white={color == '#fff'}
+		class="z-0 absolute inset-0 flex flex-row items-center mix-blend-difference justify-end px-2 text-xs text-white"
 	>
 		{Math.round(value * 100)}%
 	</div>
