9fbc23941fd2c78605fdbb45dcf8d15d933a0868
Tomáš Hobza <xhobza03@vutbr.cz>
2023-11-05 13:18:42 +0100

    add: progressbar fix

--- client/src/lib/ProgressBar.svelte
+++ client/src/lib/ProgressBar.svelte
@@ -1,16 +1,21 @@
 <script lang="ts">
 	export let value = 0;
 	export let color = '#51E5FF';
+
+	export let hasWhiteBg = false;
 </script>
 
 <div class="z-0 w-full h-full relative rounded-full border border-[#2f2f2f] overflow-hidden">
 	<div
-		class="-z-10 h-full border-r border-[#2f2f2f] transition-all duration-300"
-		style="width: {value * 100}%; background-color: {color}"
+		class="-z-10 origin-center h-full border-r box-border border-[#2f2f2f] transition-all duration-300"
+		style="width: {value * 101}%; background-color: {color}"
 	/>
 
 	<div
-		class="z-0 absolute inset-0 flex flex-row items-center mix-blend-difference justify-end px-2 text-xs text-white"
+		class="z-0 absolute inset-0 flex flex-row items-center justify-end px-2 text-xs"
+		class:text-white={!hasWhiteBg}
+		class:mix-blend-difference={!hasWhiteBg}
+		class:text-black={hasWhiteBg}
 	>
 		{Math.round(value * 100)}%
 	</div>
