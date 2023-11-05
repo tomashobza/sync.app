bb07b766ee5dded08704ed179ab0af5e58fe1932
Nikita Koliada <xkolia00@vutbr.cz>
2023-11-05 12:40:31 +0100

    add: border to UsersProfile

--- client/src/lib/UsersProfile.svelte
+++ client/src/lib/UsersProfile.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	export let photoUrl: string;
 	export let initials: string;
+	export let hasBorder = false;
 </script>
 
-<div class="w-full aspect-square rounded-full overflow-hidden">
+<div class="w-full aspect-square rounded-full overflow-hidden" class:border={hasBorder}>
 	{#if !photoUrl}
 		<div class="w-full h-full flex flex-row items-center justify-center bg-white">
 			<div class="text-slate-600 font-bold">{initials}</div>
